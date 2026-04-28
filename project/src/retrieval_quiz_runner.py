5db1022a307499ebe88f70461115d3a4121d0dc2
tommasonerii <rinotommaso@gmail.com>
2026-04-28 12:35:27 +0200

    bert

--- project/src/retrieval_quiz_runner.py
+++ project/src/retrieval_quiz_runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e26384ba1c10cb4772fc3d198934a245a7bb8a62073306f36dac47d58b170e2c
-size 13731
+oid sha256:cd03d47c07cdaacb045048ae8bd2f77f023be7b6ee09595a66811e64c336da67
+size 15439
