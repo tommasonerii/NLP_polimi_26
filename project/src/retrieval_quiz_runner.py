c305a99ad672931aec59df8f7a1743f67a42bac9
Giulia Mengoli <118433583+giulimengo@users.noreply.github.com>
2026-05-04 17:39:59 +0200

    math issues

--- project/src/retrieval_quiz_runner.py
+++ project/src/retrieval_quiz_runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67b90f313c34b197a1252fb7ffec4c13a667b16108f421f8607e6dae5a22dd26
-size 17450
+oid sha256:95f199931159042a90c119aad5d01ecd1318c33fd507f6fb41fae663ae46b59d
+size 19050
