16cb144eee0eeac7c6a89d7b2fa81ee434c45c75
tommasonerii <rinotommaso@gmail.com>
2026-05-10 18:27:29 +0200

    Update dense index build script

--- project/src/build_all_textbook_dense_indexes.ps1
+++ project/src/build_all_textbook_dense_indexes.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30df96306ffbd22c1b4d63005ee8554d15e2fab155f45b0af35c250d1e5649a0
-size 4414
+oid sha256:84d60bbc40274f9f2dd3fd1393540b0b48bbf1de350179f957bd3e09c124a579
+size 4494
