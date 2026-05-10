1bd271aff68871a1eaf79ac9a7276f4481a9f06b
tommasonerii <rinotommaso@gmail.com>
2026-05-10 18:25:40 +0200

    Ignore pyc files and reapply file tracking rules

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f7690dfc05d50940403053c9701f3a72c9c600f9ec93fabeeed7471200103e4
-size 9244
+oid sha256:9be97cdd5fb97dcc20186135f1ad04675ce4c440c7b7decf24465c499d37c8d5
+size 9485
--- project/src/build_all_textbook_dense_indexes.ps1
+++ project/src/build_all_textbook_dense_indexes.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71f70cc21aece0d2699dd15a090f29005e936c70df29827853b3916232d29190
-size 4261
+oid sha256:30df96306ffbd22c1b4d63005ee8554d15e2fab155f45b0af35c250d1e5649a0
+size 4414
